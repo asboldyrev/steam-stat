0e299d8dbcba751cd82d0ef701a364fd36944b00
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-20 00:54:21 +0700

    Schedule Steam store metadata refresh

--- routes/console.php
+++ routes/console.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Schedule;
 Schedule::command('steam:fetch-stats')
     ->everyFifteenMinutes()
     ->withoutOverlapping();
+
+Schedule::command('steam:sync-store-metadata')
+    ->dailyAt('04:00')
+    ->withoutOverlapping();
