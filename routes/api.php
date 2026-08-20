61eef2d6e394c12612db2298caf60e7ad4ded226
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-20 22:29:44 +0700

    Route game library through snapshot-based controller

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\ActivityController;
 use App\Http\Controllers\Api\DashboardController;
 use App\Http\Controllers\Api\GameController;
 use App\Http\Controllers\Api\GameDetailController;
+use App\Http\Controllers\Api\GameLibraryController;
 use App\Http\Controllers\Api\SyncController;
 
 Route::get('/activity', [ActivityController::class, 'index']);
@@ -15,7 +16,7 @@ Route::get('/dashboard/platform-distribution', [DashboardController::class, 'pla
 Route::get('/dashboard/recent-activity', [DashboardController::class, 'recentActivity']);
 Route::get('/dashboard/top-games', [DashboardController::class, 'topGames']);
 
-Route::get('/games', [GameController::class, 'index']);
+Route::get('/games', GameLibraryController::class);
 Route::get('/games/{game}/detail', GameDetailController::class);
 Route::get('/games/{game}', [GameController::class, 'show']);
 Route::get('/games/{game}/platform-breakdown', [GameController::class, 'platformBreakdown']);
