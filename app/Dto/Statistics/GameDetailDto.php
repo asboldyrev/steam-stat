1be043bdb8d5dfa81a158313ecdc1fc011beb767
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-20 20:49:58 +0700

    Expose artwork in game detail DTO

--- app/Dto/Statistics/GameDetailDto.php
+++ app/Dto/Statistics/GameDetailDto.php
@@ -9,6 +9,7 @@ use JsonSerializable;
 final readonly class GameDetailDto implements JsonSerializable
 {
     /**
+     * @param array<string,mixed> $artwork
      * @param array{total_minutes:int,windows_minutes:int,deck_minutes:int,linux_minutes:int,mac_minutes:int,unclassified_minutes:int,disconnected_minutes:int} $lifetime
      * @param array{from:string,to:string,days:int} $period
      * @param array{total_minutes:int,active_days:int,average_minutes_per_active_day:int} $summary
@@ -19,6 +20,7 @@ final readonly class GameDetailDto implements JsonSerializable
         public int $id,
         public int $appId,
         public string $name,
+        public array $artwork,
         public ?string $iconUrl,
         public ?string $coverUrl,
         public string $storeUrl,
@@ -36,6 +38,7 @@ final readonly class GameDetailDto implements JsonSerializable
             'id' => $this->id,
             'app_id' => $this->appId,
             'name' => $this->name,
+            'artwork' => $this->artwork,
             'icon_url' => $this->iconUrl,
             'cover_url' => $this->coverUrl,
             'store_url' => $this->storeUrl,
