dde93cf23aa73da75dd2bb4fc7f80ed5d41d232a
Andrew Boldyrev <angelcorpc@ya.ru>
2026-08-20 19:01:16 +0700

    Configure optional SteamGridDB integration

--- config/steam-stat.php
+++ config/steam-stat.php
@@ -4,4 +4,8 @@ declare(strict_types=1);
 
 return [
     'timezone' => env('STEAM_STAT_TIMEZONE', env('APP_TIMEZONE', 'UTC')),
+
+    'steamgriddb' => [
+        'api_key' => env('STEAMGRIDDB_API_KEY'),
+    ],
 ];
